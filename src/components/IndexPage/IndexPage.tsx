085bc105eeb3b308e1fc899846353a6eb30c07a9
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2020-12-09 20:44:07 -0800

    IndexPage component and route started.

---
+++ src/components/IndexPage/IndexPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+export const IndexPage: React.FC = () => {
+    return (
+        <div className="container">
+            <h1>Education Showwcase</h1>
+        </div>
+    )
+}
--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,10 +1,14 @@
 import React from 'react'; 
 import { Switch, Route, Redirect } from 'react-router-dom'; 
+import Home from '../components/Home/Home'; 
+import { IndexPage } from '../components/IndexPage/IndexPage'; 
 
 const routes = () => (
     <Switch >
         {/* HOME */}
-        <Route exact path='/' />
+            <Route exact path='/' component={Home}/>
+        {/* INDEX PAGE */}
+            <Route exact path='/index' component={IndexPage}/>
     </Switch>
 )
 
