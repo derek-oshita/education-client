246027a5692281385b93c59d7c038d6a24aa56ca
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2020-12-10 23:11:35 -0800

    Modal opening closer to the center with new element.

--- src/components/IndexPage/IndexPage.tsx
+++ src/components/IndexPage/IndexPage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import ShowName from '../ShowName/ShowName'; 
 import PostList from '../PostList/PostList'; 
 import Modal from '../Modal/Modal'; 
+import ModalContent from '../Modal/ModalContent'; 
 import { withRouter, useParams } from 'react-router-dom'; 
 import './IndexPage.css'; 
 
@@ -9,24 +10,46 @@ interface RouteParams {
     id: Name; 
 }
 
+// consider another element to replace w/ the modal
+
+/* <ShowName name={params.id}/> */
+
+/* <Modal modalOpen={modalOpen}>
+<ModalContent setModalOpen={setModalOpen} />
+</Modal> */
+
+/* 
+<div className="container">
+<button onClick={() => setModalOpen(true)} className="btn">Create</button></div>
+*/
+
+
 const IndexPage: React.FC = () => {
     const [modalOpen, setModalOpen] = useState(false)
 
     const params  = useParams<RouteParams>(); 
     return(
+        <>
         <div className="index-container">
             <PostList />
-            <div className="table">
-                <ShowName name={params.id}/>
+                <div className="table">
+                    <ShowName name={params.id}/>
+                        <div className="container">
+                            <button onClick={() => setModalOpen(true)} className="btn">Create</button>
+                        </div>
 
-                {/* OPEN MODAL BUTTON */}
-                <div className="container">
-                    <button onClick={() => setModalOpen(true)} className="btn">Create</button>
+                        <section className="example" >
+                            {/* <ShowName name={params.id}/>
+                            <div className="container">
+                                <button onClick={() => setModalOpen(true)} className="btn">Create</button>
+                            </div> */}
+                            <Modal modalOpen={modalOpen}>
+                                <ModalContent setModalOpen={setModalOpen} />
+                            </Modal> 
+                        </section>
                 </div>
-
-            </div>
-            <Modal modalOpen={modalOpen}/>
         </div>
+        </>
     )
 }
 
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,13 +3,31 @@ import { createPortal } from 'react-dom';
 import { withRouter, RouteComponentProps } from 'react-router-dom'; 
 import './Modal.css'; 
 import ModalContent from './ModalContent'; 
+import styled from 'styled-components'; 
+
+const Wrapper = styled.div`
+    positon: absolute; 
+    top: 0; 
+    left: 0; 
+    display: flex; 
+    align-items: center; 
+    justify-content: center; 
+    width: 100vw; 
+    height: 100vh; 
+    background: rgba(0,0,0,0.1); 
+`
+
+// with createPortal , document.body it shoves it to the bottom
+// without, it forces it to the side???
 
 const Modal: React.FC<ModalProps & RouteComponentProps> = ({ modalOpen, children }) => {
     if (!modalOpen) return null; 
     
-    return createPortal (
-        <ModalContent setModalOpen={() => false }/>, 
-        document.body
+    return (
+        <Wrapper>
+            {/* <ModalContent setModalOpen={() => false }/> */}
+            {children}
+        </Wrapper>
     )
 }
 
