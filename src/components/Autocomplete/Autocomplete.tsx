7fa2482b7f18112e995672a72dbd6012da0a583d
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2020-12-11 17:42:56 -0800

    getSuggestions causing some issues.

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -5,26 +5,35 @@ import Paper from "@material-ui/core/Paper";
 import MenuItem from "@material-ui/core/MenuItem";
 
 import './Autocomplete.css'; 
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, Observable } from "rxjs";
+
+
+interface Props<S> {
+  getSuggestions: <S>(subject: BehaviorSubject<string>) => Observable<S[]>;
+  renderSuggestion?: (suggestion: S) => JSX.Element | string;
+  onSelect?: (suggestion: S) => void;
+}
 
 // observable that allows us to conver values from React's onChange event handler into a stream of values
 const subject$ = new BehaviorSubject(''); 
 
 
-
-
-export const Autocomplete: React.FC = () => {
+export function Autocomplete<S>(props: Props<S>) {
+  const { renderSuggestion = (s: S) => s, onSelect, getSuggestions } = props; 
   const [value, setValue] = React.useState(''); 
-  const [suggestions, setSuggestions] = React.useState([]); 
+  const [suggestions, setSuggestions] = React.useState<S[]>([]); 
 
 
   // callback to every change in input field
   React.useEffect(() => {
-    const subscription = subject$.subscribe(
-      value => {
+    const subscription = getSuggestions<S>(subject$).subscribe(
+      suggestions => {
       // store new values in state
-      }, error => {
-        // handle new error
+      setSuggestions(suggestions)
+      }, 
+      error => {
+        // handle error
+        console.error(error); 
       }
     ); 
     return () => subscription.unsubscribe(); 
@@ -38,21 +47,39 @@ export const Autocomplete: React.FC = () => {
   }
 
 
-  const renderSuggestion = (suggestion: any) => {
-    // consider changing the key to another value like country
-  return <MenuItem key={suggestion.name}>{suggestion.name}</MenuItem>
+  const handleSelect = (idx: number) => {
+    if (onSelect) {
+      onSelect(suggestions[idx]); 
+      setSuggestions([]); 
+    }
   }
 
 
-  const hasSuggestions = suggestions.length > 0; 
+  // const renderSuggestion = (suggestion: any) => {
+  //   // consider changing the key to another value like country
+  // return <MenuItem key={suggestion.name}>{suggestion.name}</MenuItem>
+  // }
+
+  const shouldShowSuggestions = suggestions.length > 0 && value.length > 2;
 
 
   return(
     <div>
-      
+
       <input className="autocomplete" type="text" onChange={handleChange} value={value} />
 
-      {hasSuggestions && <Paper>suggestions.map(renderSuggestion)</Paper>}
+      {shouldShowSuggestions && (
+        <Paper>
+          {suggestions.map((suggestion, idx) => (
+            <MenuItem
+              key={`suggestion-${idx}`}
+              onClick={() => handleSelect(idx)}
+            >
+              {renderSuggestion(suggestion)}
+            </MenuItem>
+          ))}
+        </Paper>
+      )}
 
 
     </div>
--- src/components/Autocomplete/Suggestion-services.tsx
+++ src/components/Autocomplete/Suggestion-services.tsx
@@ -1,6 +1,12 @@
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { ajax, AjaxResponse } from 'rxjs/ajax';
 import { map, filter, switchMap, debounceTime } from 'rxjs/operators';
+
+export interface Suggestion {
+  name: string; 
+}
+
+
 
 const getApiUrl = (value: string) => `http://universities.hipolabs.com/search?name=${value}`
 
@@ -10,17 +16,39 @@ const transformResponse = ({ response }: AjaxResponse) => {
   }));
 };
 
-export const getSuggestions = (subject: BehaviorSubject<string>) => {
+// export const getSuggestions = <S>(subject: BehaviorSubject<string>): Observable<S[]> => {
+//   return subject.pipe(
+//     // time after user stops typing
+//     debounceTime(200), 
+//     // send request if there are 1 or more suggestions
+//     filter(v => v.length > 0), 
+//     // form url for the api call
+//     map(getApiUrl), 
+//     // call http endpoint and cancel previous requests
+//     switchMap(url => ajax(url)), 
+//     // change response shape for autocomplete 
+//     map(transformResponse)
+//   )
+// }; 
+
+// export const getSuggestions = <S>(subject: BehaviorSubject<string>): Observable<S[]> => {
+//     return subject.pipe(
+//       debounceTime(200), 
+//       filter(v => v.length > 2 ), 
+//       map(getApiUrl), 
+//       switchMap(url => ajax(url)), 
+//       map(transformResponse)
+//     )
+//   }
+
+export const getSuggestions = <S>(
+  subject: BehaviorSubject<string>
+): Observable<S[]> => {
   return subject.pipe(
-    // time after user stops typing
-    debounceTime(200), 
-    // send request if there are 1 or more suggestions
-    filter(v => v.length > 0), 
-    // form url for the api call
-    map(getApiUrl), 
-    // call http endpoint and cancel previous requests
-    switchMap(url => ajax(url)), 
-    // change response shape for autocomplete 
+    debounceTime(500),
+    filter(v => v.length > 2),
+    map(getApiUrl),
+    switchMap(url => ajax(url)),
     map(transformResponse)
-  )
-}; 
+  );
+};
